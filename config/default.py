2a99ae255b48fedcd9d23d021125a516e769a5de
Hussain Chinoy <ghchinoy@google.com>
2025-10-02 23:18:13 -0600

    chore: version

--- config/default.py
+++ config/default.py
@@ -53,7 +53,7 @@ class NavConfig(BaseModel):
 class Default:
     """Defaults class"""
 
-    VERSION: str = "1.0.5" # veo modes
+    VERSION: str = "1.0.6" # nano-banana GA
     APP_ENV: str = os.environ.get("APP_ENV", "")
 
     SERVICE_ACCOUNT_EMAIL: str = os.environ.get("SERVICE_ACCOUNT_EMAIL")
