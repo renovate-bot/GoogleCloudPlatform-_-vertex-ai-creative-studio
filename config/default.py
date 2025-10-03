ae28fa14f620f1dda1af229252e66b97bcec2f9e
Hussain Chinoy <ghchinoy@google.com>
2025-10-03 07:42:02 -0600

    chore: version

--- config/default.py
+++ config/default.py
@@ -53,7 +53,7 @@ class NavConfig(BaseModel):
 class Default:
     """Defaults class"""
 
-    VERSION: str = "1.0.6" # nano-banana GA
+    VERSION: str = "1.0.7" # Gemini TTS GA
     APP_ENV: str = os.environ.get("APP_ENV", "")
 
     SERVICE_ACCOUNT_EMAIL: str = os.environ.get("SERVICE_ACCOUNT_EMAIL")
