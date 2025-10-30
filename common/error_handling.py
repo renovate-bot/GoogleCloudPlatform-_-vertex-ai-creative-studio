74c1afab9e765732d733f6958e04df2af56cb368
Hussain Chinoy <ghchinoy@google.com>
2025-10-29 21:05:32 -0600

    refactor: logging, error handling

--- common/error_handling.py
+++ common/error_handling.py
@@ -1,3 +1,17 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import logging
 
 # Dedicated logger for tracking the suppressed error
@@ -9,6 +23,10 @@ class GenerationError(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
+
+class AsyncVeoPollingFailedError(Exception):
+    """Exception for failures during async Veo job polling."""
+    pass
 
 class UnknownHandlerIdFilter(logging.Filter):
     """A logging filter to suppress 'Unknown handler id' errors."""
