fe4c9e65722cd63b2cc50e22311b89ea2950baf7
Hussain Chinoy <ghchinoy@google.com>
2025-12-13 09:25:08 -0700

    feat: updates look and feel of gemini tts and chirp pages

--- config/default.py
+++ config/default.py
@@ -53,7 +53,7 @@ class NavConfig(BaseModel):
 class Default:
     """Defaults class"""
 
-    VERSION: str = "1.3.13" # veo extend action
+    VERSION: str = "1.3.14" # Gemini TTS models update
     APP_ENV: str = os.environ.get("APP_ENV", "")
     API_BASE_URL: str = os.environ.get(
         "API_BASE_URL", f"http://localhost:{os.environ.get('PORT', '8080')}"
